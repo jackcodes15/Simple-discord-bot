62c279470997c2180994eb7a55a2f4fe843e2ceb
Elite <77020817+elitecodesx14@users.noreply.github.com>
2024-01-31 17:08:26 +0000

    Add files via upload

---
+++ Discord bot for github/responses2.py
@@ -0,0 +1,9 @@
+def get_response(message: str) -> str:
+    p_message = message.lower()
+
+# Capitalisation will not matter with messages.
+
+# Example message below on the format you should use.
+
+    if p_message == 'Hello':
+        return 'Hello to you too!'
